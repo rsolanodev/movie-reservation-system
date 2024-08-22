4f086274f864e148849a97fbb7683fb3253a6518
Rubén Solano <41839257+rsolanodev@users.noreply.github.com>
2024-08-23 00:05:43 +0200

    🎦 Add action to update movie (#8)

--- app/movies/domain/repositories/movie_repository.py
+++ app/movies/domain/repositories/movie_repository.py
@@ -1,7 +1,10 @@
 from typing import Protocol
+from uuid import UUID
 
 from app.movies.domain.entities import Movie
 
 
 class MovieRepository(Protocol):
     def save(self, movie: Movie) -> None: ...
+
+    def get(self, id: UUID) -> Movie: ...
