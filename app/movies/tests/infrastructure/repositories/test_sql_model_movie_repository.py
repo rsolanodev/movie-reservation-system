eb2dbd75b436675ff2103dcf81c57711246af83f
rsolanodev <rubensoljim@gmail.com>
2024-08-22 18:03:51 +0200

    ✅ Add test to update movie in movie repository

--- app/movies/tests/infrastructure/repositories/test_sql_model_movie_repository.py
+++ app/movies/tests/infrastructure/repositories/test_sql_model_movie_repository.py
@@ -8,10 +8,25 @@ from app.movies.tests.factories.movie_factory import MovieFactory
 
 
 class TestSqlModelMovieRepository:
-    def test_create_movie(self, session: Session) -> None:
+    def test_creates_movie(self, session: Session) -> None:
         movie = MovieFactory().create()
         SqlModelMovieRepository(session=session).save(movie=movie)
 
         movie_model = session.get(MovieModel, movie.id)
         assert movie_model is not None
         assert movie_model.to_domain() == movie
+
+    def test_updates_movie(self, session: Session) -> None:
+        movie = MovieFactory().create()
+        movie.title = "The Super Mario Bros. Movie"
+        movie.description = "An animated adaptation of the video game."
+        movie.poster_image = "super_mario_bros.jpg"
+
+        SqlModelMovieRepository(session=session).save(movie=movie)
+
+        movie_model = session.get(MovieModel, movie.id)
+
+        assert movie_model is not None
+        assert movie_model.title == "The Super Mario Bros. Movie"
+        assert movie_model.description == "An animated adaptation of the video game."
+        assert movie_model.poster_image == "super_mario_bros.jpg"
