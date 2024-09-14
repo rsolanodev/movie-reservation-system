fdb83c38c961f784188fdd8d3c1696ab1be4e985
Rubén Solano <41839257+rsolanodev@users.noreply.github.com>
2024-09-14 23:10:56 +0200

    ⏱️ Add action to create showtime (#33)

---
+++ app/showtimes/tests/actions/test_create_showtime.py
@@ -0,0 +1,56 @@
+from datetime import datetime
+from typing import Any
+from unittest.mock import ANY, Mock, create_autospec
+from uuid import UUID
+
+import pytest
+
+from app.showtimes.actions.create_showtime import CreateShowtime, CreateShowtimeParams
+from app.showtimes.domain.entities import Showtime
+from app.showtimes.domain.exceptions import ShowtimeAlreadyExistsException
+from app.showtimes.domain.repositories.showtime_repository import ShowtimeRepository
+
+
+class TestCreateShowtime:
+    @pytest.fixture
+    def mock_repository(self) -> Any:
+        return create_autospec(ShowtimeRepository, instance=True)
+
+    def test_creates_showtime(self, mock_repository: Mock) -> None:
+        mock_repository.exists.return_value = False
+
+        CreateShowtime(repository=mock_repository).execute(
+            params=CreateShowtimeParams(
+                movie_id=UUID("cbdd7b54-c561-4cbb-a55f-15853c60e600"),
+                show_datetime=datetime(2023, 4, 2, 20, 0),
+            )
+        )
+
+        mock_repository.exists.assert_called_once_with(
+            movie_id=UUID("cbdd7b54-c561-4cbb-a55f-15853c60e600"),
+            show_datetime=datetime(2023, 4, 2, 20, 0),
+        )
+        mock_repository.create.assert_called_once_with(
+            showtime=Showtime(
+                id=ANY,
+                movie_id=UUID("cbdd7b54-c561-4cbb-a55f-15853c60e600"),
+                show_datetime=datetime(2023, 4, 2, 20, 0),
+            )
+        )
+
+    def test_does_not_create_showtime_when_exists(self, mock_repository: Mock) -> None:
+        mock_repository.exists.return_value = True
+
+        with pytest.raises(ShowtimeAlreadyExistsException):
+            CreateShowtime(repository=mock_repository).execute(
+                params=CreateShowtimeParams(
+                    movie_id=UUID("cbdd7b54-c561-4cbb-a55f-15853c60e600"),
+                    show_datetime=datetime(2023, 4, 2, 20, 0),
+                )
+            )
+
+        mock_repository.exists.assert_called_once_with(
+            movie_id=UUID("cbdd7b54-c561-4cbb-a55f-15853c60e600"),
+            show_datetime=datetime(2023, 4, 2, 20, 0),
+        )
+        mock_repository.create.assert_not_called()
