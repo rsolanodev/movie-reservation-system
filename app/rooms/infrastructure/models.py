f45bf22b5c035f1ae168b201e3ed5a2b445d59f0
rsolanodev <rubensoljim@gmail.com>
2024-10-09 21:59:10 +0200

    🍿 Fix type for seat configuration

--- app/rooms/infrastructure/models.py
+++ app/rooms/infrastructure/models.py
@@ -6,4 +6,4 @@ from sqlmodel import JSON, Column, Field, SQLModel
 class RoomModel(SQLModel, table=True):
     id: uuid.UUID = Field(default_factory=uuid.uuid4, primary_key=True)
     name: str = Field(max_length=255)
-    seat_configuration: list[dict[str, str]] = Field(sa_column=Column(JSON))
+    seat_configuration: list[dict[str, int]] = Field(sa_column=Column(JSON))
