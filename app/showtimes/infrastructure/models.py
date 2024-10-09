8c7c4ba69342c1f47f10b1308f4d6e2119097672
Rubén Solano <41839257+rsolanodev@users.noreply.github.com>
2024-10-09 21:25:29 +0200

    🍿 Add room and seat models (#50)

--- app/showtimes/infrastructure/models.py
+++ app/showtimes/infrastructure/models.py
@@ -12,8 +12,8 @@ if TYPE_CHECKING:
 
 class ShowtimeModel(SQLModel, table=True):
     id: uuid.UUID = Field(default_factory=uuid.uuid4, primary_key=True)
-    movie_id: uuid.UUID = Field(foreign_key="moviemodel.id")
     show_datetime: datetime
+    movie_id: uuid.UUID = Field(foreign_key="moviemodel.id")
     movie: "MovieModel" = Relationship(back_populates="showtimes")
 
     @classmethod
