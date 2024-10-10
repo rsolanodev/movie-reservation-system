f4c7a97d0059e352161c556f05f6a6286253724d
rsolanodev <rubensoljim@gmail.com>
2024-10-10 14:47:19 +0200

    renames

---
+++ app/auth/domain/token.py
@@ -0,0 +1,37 @@
+from dataclasses import dataclass
+from datetime import datetime, timedelta, timezone
+from enum import StrEnum
+from uuid import UUID
+
+import jwt
+
+
+class TokenType(StrEnum):
+    BEARER = "bearer"
+
+
+@dataclass
+class Token:
+    access_token: str
+    token_type: TokenType
+    expires_in: int
+
+    @classmethod
+    def create(
+        cls,
+        user_id: UUID,
+        secret_key: str,
+        expire_minutes: int,
+        token_type: TokenType = TokenType.BEARER,
+    ) -> "Token":
+        expire_date = datetime.now(tz=timezone.utc) + timedelta(minutes=expire_minutes)
+        access_token = jwt.encode(
+            payload={"exp": expire_date, "sub": str(user_id)},
+            key=secret_key,
+            algorithm="HS256",
+        )
+        return Token(
+            access_token=access_token,
+            token_type=token_type,
+            expires_in=expire_minutes,
+        )
