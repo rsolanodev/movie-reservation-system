82a924f92fc7990969fa7d74824c534f6cfcb595
rsolanodev <rubensoljim@gmail.com>
2024-10-20 00:01:44 +0200

    change

--- app/reservations/application/reservation_release.py
+++ app/reservations/application/reservation_release.py
@@ -5,12 +5,12 @@ from app.reservations.domain.repositories.reservation_repository import Reservat
 
 class ReservationRelease:
     def __init__(self, repository: ReservationRepository) -> None:
-        self.repository = repository
+        self._repository = repository
 
     def execute(self, reservation_id: UUID) -> None:
-        reservation = self.repository.get(reservation_id)
+        reservation = self._repository.get(reservation_id)
 
         if not reservation.has_paid:
             return
 
-        self.repository.release(reservation_id)
+        self._repository.release(reservation_id)
