02226e1e94a0e10020bfd00c9cb0954d4507cd02
rsolanodev <rubensoljim@gmail.com>
2024-10-19 22:04:09 +0200

    remove

--- app/reservations/infrastructure/tasks/release_reservation_task.py
+++
@@ -1,9 +0,0 @@
-from uuid import UUID
-
-from app.celery import app
-from app.reservations.application.reservation_release import ReservationRelease
-
-
-@app.task
-def reservation_release_task(reservation_id: str) -> None:
-    ReservationRelease().execute(reservation_id=UUID(reservation_id))
