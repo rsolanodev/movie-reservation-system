58f8700d6c3ec9f58a0468004b2cc603b3e71c63
Rubén Solano <41839257+rsolanodev@users.noreply.github.com>
2024-11-24 12:58:46 +0100

    👤 Integration tests for users (#81)

--- app/users/tests/infrastructure/api/test_endpoints.py
+++ app/users/tests/infrastructure/api/test_endpoints.py
@@ -1,5 +1,5 @@
 from collections.abc import Generator
-from unittest.mock import Mock, patch
+from unittest.mock import ANY, Mock, patch
 
 import pytest
 from fastapi.testclient import TestClient
@@ -19,6 +19,26 @@ class TestCreateUserEndpoint:
     def mock_user_repository(self) -> Generator[Mock, None, None]:
         with patch("app.users.infrastructure.api.endpoints.SqlModelUserRepository") as mock:
             yield mock.return_value
+
+    @pytest.mark.integration
+    def test_integration(self, client: TestClient) -> None:
+        response = client.post(
+            "api/v1/users/",
+            json={
+                "email": "rubensoljim@gmail.com",
+                "password": "Passw0rd!",
+                "full_name": "Rubén Solano",
+            },
+        )
+
+        assert response.status_code == 201
+        assert response.json() == {
+            "id": ANY,
+            "email": "rubensoljim@gmail.com",
+            "full_name": "Rubén Solano",
+            "is_active": True,
+            "is_superuser": False,
+        }
 
     def test_returns_201_and_calls_create_user(
         self, client: TestClient, mock_create_user: Mock, mock_user_repository: Mock
