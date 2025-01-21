5211b0f00d5893d77eea8acf81dd315ed05a5750
rsolanodev <rubensoljim@gmail.com>
2025-01-21 01:21:39 +0100

    💸 Integration test to confirm payment

--- app/payments/tests/infrastructure/api/test_webhooks.py
+++ app/payments/tests/infrastructure/api/test_webhooks.py
@@ -1,11 +1,16 @@
 from collections.abc import Generator
 from unittest.mock import Mock, patch
+from uuid import UUID
 
 import pytest
 from fastapi.testclient import TestClient
+from sqlmodel import Session
 
 from app.payments.application.commands.confirm_payment import ConfirmPaymentParams
 from app.payments.domain.exceptions import InvalidSignature, ReservationNotFound
+from app.payments.domain.reservation import ReservationStatus
+from app.shared.domain.payment_event import PaymentEvent
+from app.shared.tests.builders.sqlmodel_reservation_builder_test import SqlModelReservationBuilderTest
 
 
 class TestStripeWebhook:
@@ -13,6 +18,34 @@ class TestStripeWebhook:
     def mock_confirm_payment(self) -> Generator[Mock, None, None]:
         with patch("app.payments.infrastructure.api.webhooks.ConfirmPayment") as mock:
             yield mock
+
+    @pytest.fixture
+    def mock_stripe_client(self) -> Generator[Mock, None, None]:
+        with patch("app.payments.infrastructure.api.webhooks.StripeClient") as mock:
+            yield mock.return_value
+
+    @pytest.mark.integration
+    def test_integration(self, client: TestClient, session: Session, mock_stripe_client: Mock) -> None:
+        reservation_model = (
+            SqlModelReservationBuilderTest(session)
+            .with_id(UUID("92ab35a6-ae79-4039-85b3-e8b2b8abb27d"))
+            .with_status(ReservationStatus.PENDING.value)
+            .with_provider_payment_id("pi_3MtwBwLkdIwHu7ix28a3tqPa")
+            .build()
+        )
+
+        mock_stripe_client.verify_payment.return_value = PaymentEvent(
+            type="payment_intent.succeeded", payment_intent_id="pi_3MtwBwLkdIwHu7ix28a3tqPa"
+        )
+
+        response = client.post(
+            "api/v1/payments/stripe/",
+            content=b'{"type": "payment_intent.succeeded"}',
+            headers={"stripe-signature": "test_signature"},
+        )
+
+        assert response.status_code == 200
+        assert reservation_model.status == ReservationStatus.CONFIRMED.value
 
     def test_returns_200_and_calls_confirm_payment(self, client: TestClient, mock_confirm_payment: Mock) -> None:
         response = client.post(
